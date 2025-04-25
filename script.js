5d3e60cd4ee9e7c25d01f51dd291bf3d4e6ca75b
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-25 23:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 
-function Employee(name, age, jobTitle) {}
+  this.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  };
+}
+
+function Employee(name, age, jobTitle) {
+  // Inherit name and age from Person
+  Person.call(this, name, age); // Call Person constructor with this context
+  this.jobTitle = jobTitle;
+
+  this.jobGreet = function () {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  };
+}
+
+// Inherit prototype chain (if needed for prototype-based inheritance)
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
